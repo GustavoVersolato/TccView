5da84d3e1c42fad7f25c96db492f75ba1ecf35ab
Gustavo Versolato <versolatogustavo@gmail.com>
2023-10-11 19:33:49 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import MapPage from "./pages/MapPage";
+
+export const REACT_APP_GOOGLE_API_KEY = "AIzaSyCTr0IXrDcpaGIiSCRiil4nL0Kbq5OsZMI";
+
+function App() {
+  return <MapPage />;
+}
+
+export default App;
